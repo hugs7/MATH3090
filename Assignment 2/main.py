49eb37ba6599123db0ef7b632c4639edc5f528ac
hugs7 <burto732@gmail.com>
2024-04-08 19:00:38 +1000

    Updated main

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,14 +1,16 @@
 import bond
+import swap
 
 
-def main():
+def bonds():
     T = 3
     n = 2
     y = 0.09
     c = 0.08
     F = 100_000
 
-    present_values = bond.present_values_coupon_bearing_bond_discrete(F, T, c, y, n)
+    present_values = bond.present_values_coupon_bearing_bond_discrete(
+        F, T, c, y, n)
 
     print(present_values)
 
@@ -26,6 +28,8 @@ def main():
 
     print("The bond value at |D| is: ", val_at_d)
 
+
+def swaps():
     print("QUESTION 2a")
     bond_prices = [
         99412,
@@ -63,6 +67,27 @@ def main():
     print(f"spot: {spot_rates}")
 
     print(f"forward: {forward_rates}")
+    print()
+
+    notional = 1_000_000
+    fixed_rate = 0.065
+    floating_spread = 0.01
+
+    swap_values = swap.compute_swap_values(
+        notional, T, n, spot_rates, forward_rates, fixed_rate, floating_spread)
+
+    print(f"swap values: {swap_values}")
+    print()
+
+    sum_swap_values = sum(swap_values)
+
+    print(f"sum of swap values: {sum_swap_values}")
+
+
+def main():
+    # bonds()
+
+    swaps()
 
 
 if __name__ == "__main__":
