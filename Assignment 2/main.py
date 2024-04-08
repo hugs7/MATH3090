fcda8e62e999398288fb01e9706d74d37f2e1917
hugs7 <burto732@gmail.com>
2024-04-08 14:55:14 +1000

    Called in testing code

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -26,6 +26,44 @@ def main():
 
     print("The bond value at |D| is: ", val_at_d)
 
+    print("QUESTION 2a")
+    bond_prices = [
+        99412,
+        97339,
+        94983,
+        94801,
+        94699,
+        94454,
+        93701,
+        93674,
+        93076,
+        92814,
+        91959,
+        91664,
+        87384,
+        87329,
+        86576,
+        84697,
+        82642,
+        82350,
+        82207,
+        81725,
+    ]
+
+    num_bonds = 20
+    F = 100_000
+    c = 0.04
+    n = 1  # annual
+    T = [k for k in range(1, num_bonds + 1)]
+
+    spot_rates, forward_rates = bond.recursive_zero_coupon_yield_continuous(
+        bond_prices, F, T, c, n
+    )
+
+    print(f"spot: {spot_rates}")
+
+    print(f"forward: {forward_rates}")
+
 
 if __name__ == "__main__":
     main()
