d22fee7b85519c33c09c9d7a6ed4142256b48757
hugs7 <burto732@gmail.com>
2024-04-08 11:52:32 +1000

    fixed bug in bond value calculation

--- Assignment 2/bond.py
+++ Assignment 2/bond.py
@@ -8,7 +8,12 @@ import interest
 # ----- Zero Coupon Bonds -----
 
 
-def price_zero_coupon_bond_discrete(face_value: int, years_to_maturity: int, interest_rate: float, compounding_frequency_yr: int):
+def price_zero_coupon_bond_discrete(
+    face_value: int,
+    years_to_maturity: int,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+):
     """
     Calculate the price of a zero coupon bond using discrete compound interest.
 
@@ -28,14 +33,17 @@ def price_zero_coupon_bond_discrete(face_value: int, years_to_maturity: int, int
     """
 
     beta = interest.discrete_compound_interest_discounted(
-        interest_rate, years_to_maturity, compounding_frequency_yr)
+        interest_rate, years_to_maturity, compounding_frequency_yr
+    )
 
     price = face_value * beta
 
     return price
 
 
-def price_zero_coupon_bond_continuous(face_value: int, years_to_maturity: int, interest_rate: float):
+def price_zero_coupon_bond_continuous(
+    face_value: int, years_to_maturity: int, interest_rate: float
+):
     """
     Calculate the price of a zero coupon bond using continuous compound interest.
 
@@ -53,14 +61,17 @@ def price_zero_coupon_bond_continuous(face_value: int, years_to_maturity: int, i
     """
 
     beta = interest.continuous_compound_interest_discounted(
-        interest_rate, years_to_maturity)
+        interest_rate, years_to_maturity
+    )
 
     price = face_value * beta
 
     return price
 
 
-def price_zero_coupon_bond_nonconstant_yield(face_value: int, years_to_maturity: int, yield_function):
+def price_zero_coupon_bond_nonconstant_yield(
+    face_value: int, years_to_maturity: int, yield_function
+):
     """
     Calculate the price of a zero coupon bond with a nonconstant yield.
 
@@ -77,16 +88,23 @@ def price_zero_coupon_bond_nonconstant_yield(face_value: int, years_to_maturity:
             The price of the bond.
     """
 
-    price = face_value * \
-        interest.nonconstant_yield_discounted(
-            yield_function, years_to_maturity)
+    price = face_value * interest.nonconstant_yield_discounted(
+        yield_function, years_to_maturity
+    )
 
     return price
 
 
 # ----- Coupon Bearing Bonds -----
 
-def price_coupon_bearing_bond_discrete(face_value: int, years_to_maturity: int, coupon_rate: float, interest_rate: float, compounding_frequency_yr: int):
+
+def price_coupon_bearing_bond_discrete(
+    face_value: int,
+    years_to_maturity: int,
+    coupon_rate: float,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+):
     """
     Calculate the price of a coupon bearing bond using discrete compound interest.
 
@@ -108,7 +126,12 @@ def price_coupon_bearing_bond_discrete(face_value: int, years_to_maturity: int, 
     """
 
     # Define subfunction for computing the present value of the coupon payments
-    def coupon_payment_present_value(coupon_rate_annual: float, interest_rate: float, compounding_frequency_yr: int, years_to_maturity: int):
+    def coupon_payment_present_value(
+        coupon_rate_annual: float,
+        interest_rate: float,
+        compounding_frequency_yr: int,
+        years_to_maturity: int,
+    ):
         """
         Calculate the present value of the coupon payments of a bond.
 
@@ -131,8 +154,9 @@ def price_coupon_bearing_bond_discrete(face_value: int, years_to_maturity: int, 
 
         coupon_rate_adjusted = coupon_rate_annual / compounding_frequency_yr
 
-        numerator = (1 - interest.discrete_compound_interest_discounted(
-            interest_rate, years_to_maturity, compounding_frequency_yr))
+        numerator = 1 - interest.discrete_compound_interest_discounted(
+            interest_rate, years_to_maturity, compounding_frequency_yr
+        )
 
         denominator = interest_rate / compounding_frequency_yr
 
@@ -148,12 +172,14 @@ def price_coupon_bearing_bond_discrete(face_value: int, years_to_maturity: int, 
 
     # Add the present value of the coupon payments
     coupon_value = coupon_payment_present_value(
-        coupon_rate_annual, interest_rate, compounding_frequency_yr, years_to_maturity)
+        coupon_rate_annual, interest_rate, compounding_frequency_yr, years_to_maturity
+    )
     price += coupon_value
 
     # Add the face value discounted to the present
     discounted_face_value = price_zero_coupon_bond_discrete(
-        face_value, years_to_maturity, interest_rate, compounding_frequency_yr)
+        face_value, years_to_maturity, interest_rate, compounding_frequency_yr
+    )
     price += discounted_face_value
 
     return price
@@ -179,7 +205,9 @@ def adjusted_coupon_rate(coupon_rate: float, compounding_frequency_yr: int) -> f
     return adjusted_coupon_rate
 
 
-def coupon_value(face_value: int, coupon_rate: float, compounding_frequency_yr: int) -> float:
+def coupon_value(
+    face_value: int, coupon_rate: float, compounding_frequency_yr: int
+) -> float:
     """
     Calculate the value of a coupon payment.
 
@@ -196,13 +224,21 @@ def coupon_value(face_value: int, coupon_rate: float, compounding_frequency_yr: 
             The value of the coupon payment.
     """
 
-    coupon_value = face_value * \
-        adjusted_coupon_rate(coupon_rate, compounding_frequency_yr)
+    coupon_value = face_value * adjusted_coupon_rate(
+        coupon_rate, compounding_frequency_yr
+    )
 
     return coupon_value
 
 
-def present_value_coupon_bearing_bond_discrete(time_step: int, face_value: int, years_to_maturity: int, coupon_rate: float, interest_rate: float, compounding_frequency_yr: int) -> float:
+def present_value_coupon_bearing_bond_discrete(
+    time_step: int,
+    face_value: int,
+    years_to_maturity: int,
+    coupon_rate: float,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+) -> float:
     """
     Calculate the present value of the coupon payments of a bond at a specific time step.
 
@@ -226,16 +262,17 @@ def present_value_coupon_bearing_bond_discrete(time_step: int, face_value: int, 
     """
 
     if time_step < 1:
-        raise ValueError(
-            "Time step must be greater than or equal to 1.")
+        raise ValueError("Time step must be greater than or equal to 1.")
 
     num_time_steps = years_to_maturity * compounding_frequency_yr
 
     adjusted_coupon_value = coupon_value(
-        face_value, coupon_rate, compounding_frequency_yr)
+        face_value, coupon_rate, compounding_frequency_yr
+    )
 
     beta = interest.discrete_interest_bond(
-        interest_rate, time_step, compounding_frequency_yr)
+        interest_rate, time_step, compounding_frequency_yr
+    )
 
     if time_step < num_time_steps:
         return adjusted_coupon_value * beta
@@ -243,7 +280,13 @@ def present_value_coupon_bearing_bond_discrete(time_step: int, face_value: int, 
         return (adjusted_coupon_value + face_value) * beta
 
 
-def present_values_coupon_bearing_bond_discrete(face_value: int, years_to_maturity: int, coupon_rate: float, interest_rate: float, compounding_frequency_yr: int) -> list[float]:
+def present_values_coupon_bearing_bond_discrete(
+    face_value: int,
+    years_to_maturity: int,
+    coupon_rate: float,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+) -> list[float]:
     """
     Calculate the present values of the coupon payments of a bond at each time step.
 
@@ -266,13 +309,28 @@ def present_values_coupon_bearing_bond_discrete(face_value: int, years_to_maturi
 
     num_time_steps = years_to_maturity * compounding_frequency_yr
 
-    present_values = [present_value_coupon_bearing_bond_discrete(
-        time_step, face_value, years_to_maturity, coupon_rate, interest_rate, compounding_frequency_yr) for time_step in range(1, num_time_steps + 1)]
+    present_values = [
+        present_value_coupon_bearing_bond_discrete(
+            time_step,
+            face_value,
+            years_to_maturity,
+            coupon_rate,
+            interest_rate,
+            compounding_frequency_yr,
+        )
+        for time_step in range(1, num_time_steps + 1)
+    ]
 
     return present_values
 
 
-def bond_duration_discrete(face_value: int, years_to_maturity: int, coupon_rate: float, interest_rate: float, compounding_frequency_yr: int) -> float:
+def bond_duration_discrete(
+    face_value: int,
+    years_to_maturity: int,
+    coupon_rate: float,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+) -> float:
     """
     Calculate the duration of a bond with discrete interest.
 
@@ -297,21 +355,40 @@ def bond_duration_discrete(face_value: int, years_to_maturity: int, coupon_rate:
 
     num_time_steps = years_to_maturity * compounding_frequency_yr
 
-    B = sum(present_values_coupon_bearing_bond_discrete(
-        face_value, years_to_maturity, coupon_rate, interest_rate, compounding_frequency_yr))
+    B = sum(
+        present_values_coupon_bearing_bond_discrete(
+            face_value,
+            years_to_maturity,
+            coupon_rate,
+            interest_rate,
+            compounding_frequency_yr,
+        )
+    )
 
     for time_step in range(1, num_time_steps + 1):
         year = time_step / compounding_frequency_yr
 
         present_value_t = present_value_coupon_bearing_bond_discrete(
-            time_step, face_value, years_to_maturity, coupon_rate, interest_rate, compounding_frequency_yr)
+            time_step,
+            face_value,
+            years_to_maturity,
+            coupon_rate,
+            interest_rate,
+            compounding_frequency_yr,
+        )
 
-        duration += ((year * present_value_t) / B)
+        duration += (year * present_value_t) / B
 
     return duration
 
 
-def bond_value_at_time(bond_duration: float, face_value: int, years_to_maturity: int, interest_rate: float, compounding_frequency_yr: int) -> float:
+def bond_value_at_time(
+    bond_duration: float,
+    face_value: int,
+    years_to_maturity: int,
+    interest_rate: float,
+    compounding_frequency_yr: int,
+) -> float:
     """
     Calculates the value of a bond at a specific time.
 
@@ -333,43 +410,41 @@ def bond_value_at_time(bond_duration: float, face_value: int, years_to_maturity:
     """
 
     if bond_duration < 0 or bond_duration > years_to_maturity:
-        raise ValueError(
-            "Bond duration must be in the range [0, years_to_maturity].")
+        raise ValueError("Bond duration must be in the range [0, years_to_maturity].")
 
     if compounding_frequency_yr < 1:
-        raise ValueError(
-            "Compounding frequency must be a positive integer.")
+        raise ValueError("Compounding frequency must be a positive integer.")
 
     if interest_rate < 0 or interest_rate > 1:
-        raise ValueError(
-            "Interest rate must be in the range [0, 1].")
+        raise ValueError("Interest rate must be in the range [0, 1].")
 
     num_time_steps = years_to_maturity * compounding_frequency_yr
+    t_1 = num_time_steps - 1
 
     interest_adjusted = 1 + interest_rate
-    coup_val = coupon_value(face_value, interest_rate,
-                            compounding_frequency_yr)
+    coup_val = coupon_value(face_value, interest_rate, compounding_frequency_yr)
     bond_value = 0
 
     # Coupons
 
     reinvestments = []
 
-    for time_step in range(1, num_time_steps + 1):
+    for time_step in range(1, t_1 + 1):
         year = time_step / compounding_frequency_yr
 
         # Length of time this coupon can be reinvested for
         reinvestment_time = bond_duration - year
 
-        coupon_reinvestment_val = coup_val * interest_adjusted ** reinvestment_time
+        coupon_reinvestment_val = coup_val * interest_adjusted**reinvestment_time
 
         reinvestments.append(coupon_reinvestment_val)
 
     # Last coupon (including face value)
 
     # B
-    last_coupon_reinvestment_val = (
-        face_value + coup_val) / (interest_adjusted ** (years_to_maturity - bond_duration))
+    last_coupon_reinvestment_val = (face_value + coup_val) / (
+        interest_adjusted ** (years_to_maturity - bond_duration)
+    )
 
     reinvestments.append(last_coupon_reinvestment_val)
 
@@ -380,7 +455,15 @@ def bond_value_at_time(bond_duration: float, face_value: int, years_to_maturity:
 
 # ---- Yield Curves ----
 
-def spot_zero_coupon_yield_curve_continuous(face_value: int, present_value: float, years_to_maturity: int, interest_rate: float, coupon_rate: float, compounding_frequency_yr: int) -> float:
+
+def spot_zero_coupon_yield_curve_continuous(
+    face_value: int,
+    present_value: float,
+    years_to_maturity: int,
+    interest_rate: float,
+    coupon_rate: float,
+    compounding_frequency_yr: int,
+) -> float:
     """
     Computes the spot yield curve for a zero coupon bond using continuous compound interest.
 
@@ -409,8 +492,7 @@ def spot_zero_coupon_yield_curve_continuous(face_value: int, present_value: floa
             raise ValueError("Years to maturity must be non-negative.")
 
         if compounding_frequency_yr < 1:
-            raise ValueError(
-                "Compounding frequency must be a positive integer.")
+            raise ValueError("Compounding frequency must be a positive integer.")
 
         if interest_rate < 0 or interest_rate > 1:
             raise ValueError("Interest rate must be in the range [0, 1].")
@@ -425,8 +507,7 @@ def spot_zero_coupon_yield_curve_continuous(face_value: int, present_value: floa
             raise ValueError("Face value must be non-negative.")
 
         if present_value > face_value:
-            raise ValueError(
-                "Present value must be less than or equal to face value.")
+            raise ValueError("Present value must be less than or equal to face value.")
 
     coup_val = coupon_value(face_value, coupon_rate, compounding_frequency_yr)
 
@@ -439,6 +520,9 @@ def spot_zero_coupon_yield_curve_continuous(face_value: int, present_value: floa
         year = time_step / compounding_frequency_yr
 
         discount_sum += interest.continuous_compound_interest_accumulated(
-            interest_rate, year)
+            interest_rate, year
+        )
 
-    return (1 / num_time_steps) * math.log((c_f) / (present_value - coup_val * discount_sum))
+    return (1 / num_time_steps) * math.log(
+        (c_f) / (present_value - coup_val * discount_sum)
+    )
